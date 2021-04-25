1ace09024ce633c02b21160059a4b03838319e5e
Your Name <pillorajem10@gmail.com>
2021-04-25 13:23:48 +0800

    modified validator

--- validator/index.js
+++ validator/index.js
@@ -1,11 +1,11 @@
 exports.userSignupValidator = (req, res, next) => {
 
     req.check('name', 'Name is required').notEmpty();
-    req.check('email', 'Email must be between 3 to 32 characters')
+    req.check('email', 'Email must be between 8 to 32 characters')
         .matches(/.+\@.+\..+/)
         .withMessage('Email must contain @')
         .isLength({
-            min: 4,
+            min: 8,
             max: 32
         });
     req.check('password', 'Password is required').notEmpty();
